a169f9b7d9150ee57d578efed885fa472a3fb8d3
ryan-mant <ryan.tuglife@hotmail.com>
2025-07-13 14:20:05 -0300

    finish SECTION 5

--- src/main/java/org/example/service/PersonService.java
+++ src/main/java/org/example/service/PersonService.java
@@ -2,11 +2,18 @@ package org.example.service;
 
 import org.example.model.Person;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class PersonService implements IPersonService{
 
 
     @Override
     public Person createPerson(Person person) {
+
+        if (person.getEmail() == null || person.getEmail().isEmpty()) throw new IllegalArgumentException("The Person email is null or empty!");
+
+        var id = new AtomicLong().incrementAndGet();
+        person.setId(id);
         return person;
     }
 }
--- src/test/java/org/example/PersonServiceTest.java
+++ src/test/java/org/example/PersonServiceTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.model.Person;
 import org.example.service.IPersonService;
 import org.example.service.PersonService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,20 +12,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PersonServiceTest {
 
+    Person person;
+
+    @BeforeEach
+    void setUp() {
+         person = new Person(
+                "Lucas",
+                "Silva",
+                "Austin - TX",
+                "Male",
+                "lucas.silva@email.com"
+        );
+    }
+
+
     // test[System Under Test]_[Condition or State Change]_[Expected Result]
     @DisplayName("When Create a Person With Success Should Return a Person Object")
     @Test
     void testCreatePerson_WhenSuccess_ShouldReturnPersonObject() {
     	// Given / Arrange
         IPersonService personService = new PersonService();
-
-        Person person = new Person(
-                "Lucas",
-                "Silva",
-                "lucas.silva@email.com",
-                "Austin - TX",
-                "Male"
-        );
 
     	// When / Act
         Person actual = personService.createPerson(person);
@@ -34,19 +41,11 @@ public class PersonServiceTest {
         assertNotNull(actual, "The createPerson() should not have return null");
     }
 
-    @DisplayName("When Create a Person With Success Should Contains First Name In Returned Person Object")
+    @DisplayName("When Create a Person With Success Should Contains Valid Fields In Returned Person Object")
     @Test
-    void testCreatePerson_WhenSuccess_ShouldContainsFirstNameInReturnedPersonObject() {
+    void testCreatePerson_WhenSuccess_ShouldContainsValidFieldsInReturnedPersonObject() {
         // Given / Arrange
         IPersonService personService = new PersonService();
-
-        Person person = new Person(
-                "Lucas",
-                "Silva",
-                "lucas.silva@email.com",
-                "Austin - TX",
-                "Male"
-        );
 
         // When / Act
         Person actual = personService.createPerson(person);
@@ -55,6 +54,30 @@ public class PersonServiceTest {
 
         assertNotNull(actual, "The createPerson() should not have return null");
 
+        assertNotNull(person.getId(), "The ID is missing!");
+
         assertEquals(person.getFirstName(), actual.getFirstName(), "The first name should be equal");
+        assertEquals(person.getLastName(), actual.getLastName(), "The last name should be equal");
+        assertEquals(person.getAddress(), actual.getAddress(), "The address should be equal");
+        assertEquals(person.getEmail(), actual.getEmail(), "The e-mail should be equal");
+        assertEquals(person.getGender(), actual.getGender(), "The gender should be equal");
+    }
+
+    // test[System Under Test]_[Condition or State Change]_[Expected Result]
+    @DisplayName("When Create a Person With Null Email Should Throw Exception")
+    @Test
+    void testCreatePerson_WhithNullEmail_ShouldThrowIllegalArgumentException() {
+        // Given / Arrange
+        IPersonService personService = new PersonService();
+
+        person.setEmail(null);
+
+        // When / Act
+    	// Then / Assert
+        String message = assertThrows(IllegalArgumentException.class, () -> personService.createPerson(person),
+                () -> "Empty Email Should Have cause an IllegalArgumentException").getMessage();
+
+        assertEquals("The Person email is null or empty", message,
+                "The exception message is difference of expected");
     }
 }
